58b801cc05bd882a9aeea926c398ffc39ee38675
Cory Levinson <cory@soundcloud.com>
2017-05-04 22:19:00 +0300

    cleanup challenge 4

--- src/Main.hs
+++ src/Main.hs
@@ -6,5 +6,16 @@ import Lib
 
 
 main :: IO ()
-main = do topStrings <- loadCyphertexts "data/challenge-4.txt" >>= decryptCyphertexts
-          putStrLn $ show $ (sortByRating topStrings) !! 0
+main = challengeFour
+
+
+challengeThree :: IO ()
+challengeThree = do
+    decodedStr <- decryptSCXor
+      (decodeHex "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+    putStrLn decodedStr
+
+challengeFour :: IO ()
+challengeFour = do
+    topStrings <- loadCyphertexts "data/challenge-4.txt" >>= decryptCyphertexts
+    putStrLn $ show $ (sortByRating topStrings) !! 0
